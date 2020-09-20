07d558fb0a8af19f87508b17a9fc94b15cee1cf3
Hana Hassan <55105184+meowmeowow@users.noreply.github.com>
2020-09-19 18:23:58 -0700

    Update Main.java
    I'm just trying to upload my stuff I hope this doesn't update everything ..

--- Main.java
+++ Main.java
@@ -1,14 +1,61 @@
-package HiVolt2;
-import java.util.*;
+
 public class Main {
-	public static void main(String args[])
-	{
-		Scanner input = new Scanner(System.in);
-		Board b = new Board();
-		while (true)
-		{
-			char Char = input.next().charAt(0);
-			b.process(Char);
+	static int turn;
+	
+	public static void playGame() {
+		turn = 0;
+		Board b = new Board(10,10);
+		
+
+		Display d = new Display(b.width,b.height);
+		
+		d.displayBoard(b);
+		d.displayConsole(b);
+		while(b.player != null) {
+			turn++;
+			boolean moveRobots = b.player.movePlayer(b);			
+			
+			if (b.player == null) {
+				break;
+			}
+			
+			if (moveRobots == true) {
+				b.turn();
+			}
+			
+			d.displayBoard(b);
+			d.displayConsole(b);
+			//check if all robots are dead
+			
+			if (b.getEnemyList().size() == 0) {
+				break;
+			}
 		}
+		d.displayConsole(b);
+
+		System.out.println("Game Over!");
+		d.gameOver();
+		if(b.player == null) {
+			System.out.println("You lost :(");
+		}
+		else {
+			System.out.println("You Win :)");
+		}
+		System.out.println("You made " + turn + " turn(s)!");
+		
+		
 	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//if player is null, end game
+		
+		// in a while loop
+		System.out.println("Welcome to HiVolts!!!!");
+		System.out.println("Would you like to play: "); // get input
+		playGame();
+		System.out.println("Would you like to play again: ");// get input
+
+
+}
 }
