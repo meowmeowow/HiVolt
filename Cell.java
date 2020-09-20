acb2121336b1bf7885b0919f6ec830c8be6d72f6
Hana Hassan <55105184+meowmeowow@users.noreply.github.com>
2020-09-19 18:27:52 -0700

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,21 @@
+
+public class Cell {
+	int x;
+	int y;
+	//default
+	
+	public Cell(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String draw() {
+		//draws itself
+		return(" ");
+	}
+	
+	public void move(Board b) { 
+		//does the logic for moving
+		// checks if it dies 
+	}
+}
