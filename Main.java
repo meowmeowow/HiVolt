3243f40f874eacd80dc3171a8e221d90b89ce14d
Hana Hassan <55105184+meowmeowow@users.noreply.github.com>
2020-09-24 01:08:01 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
 
+
 public class Main {
 	static int turn;
 	
-	public static void playGame() {
-		turn = 0;
-		Board b = new Board(10,10);
-		
-
-		Display d = new Display(b.width,b.height);
-		
-		d.displayBoard(b);
-		d.displayConsole(b);
+	public static void playGame(Board b, Display d) throws InterruptedException {
+		turn = 0;		
+		d.displayBoard(b,turn);
+		boolean win;
+		//d.displayConsole(b);
 		while(b.player != null) {
 			turn++;
-			boolean moveRobots = b.player.movePlayer(b);			
+			char keypressed = d.waitForKey();
+			boolean moveRobots = b.player.movePlayer(b,keypressed);			
 			
 			if (b.player == null) {
 				break;
@@ -23,39 +21,52 @@ public class Main {
 				b.turn();
 			}
 			
-			d.displayBoard(b);
-			d.displayConsole(b);
+			d.displayBoard(b,turn);
+			//d.displayConsole(b);
 			//check if all robots are dead
 			
 			if (b.getEnemyList().size() == 0) {
 				break;
 			}
 		}
-		d.displayConsole(b);
 
 		System.out.println("Game Over!");
-		d.gameOver();
 		if(b.player == null) {
 			System.out.println("You lost :(");
+			//return ..
+			win = false;
 		}
 		else {
 			System.out.println("You Win :)");
+			//return .
+			win = true;
 		}
-		System.out.println("You made " + turn + " turn(s)!");
+		d.gameOver(win,turn);
+		//System.out.println("You made " + turn + " turn(s)!");
 		
 		
 	}
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		//if player is null, end game
+		Board b = new Board(10,10);
+		Display d = new Display(b.width,b.height);
 		
-		// in a while loop
-		System.out.println("Welcome to HiVolts!!!!");
-		System.out.println("Would you like to play: "); // get input
-		playGame();
-		System.out.println("Would you like to play again: ");// get input
-
-
+		while(true) {
+	        //System.out.println("Welcome to HiVolts!!!!");
+			// in a while loop
+			//System.out.print("Enter coin: ");// make this a input on the screen
+			d.startScreen();
+			
+			d.waitFor(); 
+			//System.out.println("play");
+			
+			playGame(b,d);
+			
+			d.waitFor();
+			
+			b = new Board(10,10);
+		}
 }
 }
